8f00e95ad74be41787812e689c10baa5459f5ba6
Yesbolat <y.ashim@jusan.kz>
2023-05-01 15:18:27 +0600

    add experimental test of wikipedia app

---
+++ src/test/java/tests/android_test/TestBase.java
@@ -0,0 +1,37 @@
+package tests.android_test;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import drivers.MobileDriver;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestBase {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browser = MobileDriver.class.getName();
+        Configuration.browserSize = null;
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        open();
+    }
+
+    @AfterEach
+    void afterEach() {
+//      Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        closeWebDriver();
+    }
+
+}
